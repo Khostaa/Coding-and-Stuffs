2ea08539d6cafc421199464d0dd5608bee58fbf5
khostaa <sunilpoudel668@gmail.com>
2023-02-01 23:14:49 +0545

    Added Code for MAX HEAP Using Descending Priority Queue in C/C++

---
+++ Algorithms/max-heap.c
@@ -0,0 +1,121 @@
+//implement max-heap as priority queue
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//function that swaps
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d, ",arr[i]);
+    }
+    printf("\n\n");
+}
+void swap(int *p,int *q)
+{
+    int t = *p; // t is temporary variable to perform swapping
+    *p = *q;
+    *q = t;
+}
+//function that finds largest among the nodes
+void max_heapify(int arr[], int i, int n)
+{
+    int largest = i; //declaring node at index i as largest 
+    int left = 2*i + 1; //node at index 2*i+1 is left node
+    int right = 2*i + 2; //node at index 2*i + 2 is right node
+
+    if (left <= n && arr[left]>arr[largest])
+        {
+            largest = left;
+        }
+    if (right <= n && arr[right]>arr[largest])
+        {
+            largest = right;
+        }
+    if (largest != i ) //if largest is not same as previous one
+    {    
+      swap(&arr[largest],&arr[i]);
+      max_heapify(arr,largest,n);
+    }
+}
+void build_max_heap(int arr[],int n)
+{
+    // builds maximum heap
+    for(int i= (n/2 - 1);i>=0;i--)
+    {
+        max_heapify(arr,i,n);
+    }
+    //Heap Sort
+    for(int i=n-1 ;i>=0;i--)
+    {
+        swap(&arr[0],&arr[i]); 
+        max_heapify(arr,i,0); // to get highest element at TOP Again
+    }
+}
+void enqueue(int arr[],int element,int n)
+{
+    //insertion into HEAP
+    arr[n] = element; // adding latest element to last
+    int largest = n; // assuming last element as largest
+    int l = floor(largest/2); //for comparing largest with its parent node
+    while(largest > 0 && arr[largest] > arr[l]) // if largest greater than parent node loop works
+    {
+        swap(&arr[largest],&arr[l]); // exchanges largest with parent node
+        largest = l;
+        l= l/2; // for comparing with predecessor parent node
+    }
+    n = n + 1; // as element is inserterd so, we increase n;
+    printf("\nHEAP After Insertion: ");
+    display(arr,n);
+}
+int dequeue(int arr[],int n)
+{
+    //as dequeue or deletion is done from the TOP
+    int temp = arr[0]; // assinging root to temporary variable
+    arr[0] = arr[n]; // root is replaced by element at last index
+    max_heapify(arr,0,n-1); // adjusting HEAP without HIGHEST PRIORITY element
+    n = n-1; // as element is removed/dequeued so, n decreases
+    return temp;
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7}; //initial heap
+    int n = sizeof(arr)/sizeof(arr[0]);// n store size of an array/heap
+    int choice,element;
+    printf("\nHeap Before Sorting: ");
+    display(arr,n);
+    build_max_heap(arr,n); // function that finds largest among left, right and root nodes
+    printf("\n Heap After Sorting as Priority Queue: ");
+    display(arr,n);
+   
+    do
+    {
+        printf("\nChoice?\n1.Enqueue\n2.Dequeue\n3.EXIT\n");
+        printf(">");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+                printf("\nEnter element to insert: ");
+                scanf("%d",&element);
+                enqueue(arr,element,n);
+                break;
+                
+            case 2:
+                printf("\n%d is removed from the HEAP.\n",dequeue(arr,n));
+                printf("\nHEAP After deletion: ");
+                display(arr,n);
+                break;
+            case 3:
+                printf("Bye Bye!\n");
+                break;
+            default:
+                printf("Enter Valid Number.!\n");
+                break;
+
+        }
+    } while (choice != 3);
+    
+    return 0;
+}
---
+++ Algorithms/pqheap.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#define size 10
+int arr[size];
+int rear=0,front=0;
+int swap(int *a,int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void heapify (int arr[],int i,int n)
+{
+    int largest = i; //declaring node at index i as largest 
+    int left = 2*i + 1; //node at index 2*i+1 is left node
+    int right = 2*i + 2; //node at index 2*i + 2 is right node
+
+    if (left <= n && arr[left]>arr[largest])
+        {
+            largest = left;
+        }
+    if (right <= n && arr[right]>arr[largest])
+        {
+            largest = right;
+        }
+    if (largest != i ) //if largest is not same as previous one
+    {    
+      swap(&arr[i],&arr[largest]);
+      heapify(arr,largest,n);
+    }
+}
+
+void enqueue(int e)
+{
+    if (rear == size ) printf("Queue is Full!\n");
+    arr[rear]= e; //add element from rear end
+    rear = rear + 1; //increase rear after insertion
+    for (int i= size/2-1;i>=0;i--)
+    {
+        heapify(arr,i,size); //for max heap
+    }
+}
+int dequeue()
+{
+    int temp;
+    if(front == rear)
+    {
+        return 0;
+    }
+    else
+    {
+        int s = sizeof(arr);
+        for(int i=0;i<s;i++)
+        {
+            if(i==0) temp = arr[i];
+            arr[i]= arr[i+1];
+        }
+        s--;
+        for (int i= s/2-1;i>=0;i--)
+        {
+            heapify(arr,i,s); //for max heap after removal of ROOT/highest priority element
+        }   
+        return temp;
+}
+    }
+  
+void display(int arr[],int s)
+{
+    s = rear; //for displaying only available elements
+    for(int i=0;i<s;i++)
+    {
+        printf("%d, ",arr[i]);
+    }
+    printf("\n\n");
+}
+int main()
+{
+    int choice,element;
+    
+    do
+    {
+        printf("\nChoice?\n1.Enqueue\n2.Dequeue\n3.Display\n4.EXIT\n");
+        printf(">");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter element to enqueue: ");
+            scanf("%d",&element);
+            enqueue(element);
+            break;
+        case 2:
+            printf("%d is dequeued.\n",dequeue());
+            break;
+        case 3:
+            display(arr,size);
+            break;
+        case 4:
+            printf("Bye Bye!\n");
+            break;
+        
+        default:
+            printf("Enter 1,2,3 or 4 only.\n");
+            break;
+        }
+     
+    } while (choice!=4);
+    return 0;
+}
